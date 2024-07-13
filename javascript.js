58c4f993fc463d86df6e0e69a03b94324367a81e
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-13 12:06:55 -0700

    Create getComputerChoice() function
    Get a random number from Math.random, multiply by 10, then round
    down. Conditional statement that redirects that number and converts
    to either rock, paper, scissors selection for the computer

--- javascript.js
+++ javascript.js
@@ -2,3 +2,14 @@ function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper or Scissors?");
     return humanChoice = humanChoice.toLowerCase();
 }
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*10);
+    if (computerChoice <= 2) {
+        computerChoice = "rock";
+    } else if (computerChoice <= 5) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+}
