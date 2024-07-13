8e0f6affdd7b4e7fbe896c2747ddafe4f03dbba1
matthart10 <170780306+matthart10@users.noreply.github.com>
2024-07-13 12:29:46 -0700

    Create playRound() function
    A set of conditionals that compares all the scenarios in which
    the computer and human have, and assigns a string whether user
    wins, loses, or ties, and adds 1 to their score variable

--- javascript.js
+++ javascript.js
@@ -16,3 +16,31 @@ function getComputerChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        humanScore = humanScore + 1;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper.");
+        computerScore = computerScore + 1;
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Tie! You both picked Scissors.");
+    } else if (humanChoice === "rock" && computerChoice === "rock") {
+        console.log("Tie! You both picked Rock.");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock.");
+        humanScore = humanScore + 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Scissors beats Rock");
+        computerScore = computerScore + 1;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock");
+        computerScore = computerScore + 1;
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie! You both picked Paper.");
+    } else {
+        console.log("You win! Scissors beats Paper.");
+        humanScore = humanScore + 1;
+    }
+}
